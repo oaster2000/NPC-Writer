95faa8312e0a38f1c8d2a7e8fda5606889421957
Owen Davies <oaster2000@gmail.com>
2023-03-13 14:05:21 +0000

    FEAT: World switching via select

--- anvil.py
+++ anvil.py
@@ -1,6 +1,18 @@
 import os, requests, json
 
+class User:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class World:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
 class Anvil:
+    current_world: World
+    
     def __init__(self):
         self.auth_token = os.getenv("WA_USER_TOKEN")
         self.application_key = os.getenv("WA_APPLICATION_KEY")
@@ -9,7 +21,8 @@ class Anvil:
             "x-application-key": self.application_key,
             "Content-type": "application/json",
             "User-Agent": "Web App v0.1"
-        }     
+        }
+        self.current_world = World("", "")
 
     def load_user(self):
         url = "https://www.worldanvil.com/api/aragorn/user"
@@ -25,13 +38,10 @@ class Anvil:
         for world in result["worlds"]:
             worlds.append(World(world["id"], world["name"]))
         return worlds
-
-class User:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-class World:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+    
+    def get_world(self, worlds, world_id):
+        for world in worlds:
+            if world.id == world_id:
+                return world
+            
+        return None
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests, os, json
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from dotenv import load_dotenv
 from anvil import Anvil, User
 
@@ -13,6 +13,19 @@ user = anvil.load_user()
 
 worlds = anvil.load_worlds(user)
 
+anvil.current_world = worlds[0]
+
 @app.route('/')
+def main():
+   return redirect(url_for('index'))
+
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', name=user.name, worlds=worlds)
+    if request.method == 'POST':
+        print(request.form["world"])
+        anvil.current_world = anvil.get_world(worlds, request.form["world"])
+        
+    return render_template('index.html', name=user.name, worlds=worlds, current_world=anvil.current_world)
+
+if __name__ == "__main__":
+    app.run(debug=True)
